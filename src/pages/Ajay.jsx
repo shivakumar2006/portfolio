860a486d505d870fa1378fcbaf8b62bfc43d27b8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-06-28 22:16:52 +0530

    commit

---
+++ src/pages/Ajay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ajay = () => {
+  return (
+    <div>Ajay</div>
+  )
+}
+
+export default Ajay
---
+++ src/pages/Shiva.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shiva = () => {
+  return (
+    <div>Shiva</div>
+  )
+}
+
+export default Shiva
