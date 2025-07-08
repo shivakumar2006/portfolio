7661cbe7685d91f245e84b5104b3577380a55deb
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-08 18:14:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Landing from "./pages/landing";
 import Shiva from "./pages/Shiva";
 import Ajay from "./pages/Ajay";
 
